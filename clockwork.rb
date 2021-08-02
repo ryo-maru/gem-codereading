26e7e8137f300f6c4dfcb73506542d7ad076f2a7
maruyamaryo <t.t.uncle0803@gmail.com>
2021-08-02 16:21:10 +0900

    first

---
+++ clockwork.rb
@@ -0,0 +1,14 @@
+require 'clockwork'
+include Clockwork
+
+handler do |job|
+  puts "Running #{job}"
+end
+
+every(10.seconds, 'frequent.job')
+every(3.minutes, 'less.frequent.job')
+every(1.hour, 'hourly.job')
+
+every(1.day, 'midnight.job', :at => '00:00')
+
+clockwork sample.rb
---
+++ sample.rb
@@ -0,0 +1,7 @@
+require "slack-notify"
+require 'clockwork'
+# ここに、slackにメッセージを送る処理を書き込む。
+sample = SlackNotify::Client.new(webhook_url: '（https://hooks.slack.com/services/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）')
+
+
+samlle = 'hello'
