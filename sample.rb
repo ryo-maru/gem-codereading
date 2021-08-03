cd4d46e2c36616b5bd8c5900e9fa5c8260e38468
maruyamaryo <t.t.uncle0803@gmail.com>
2021-08-03 15:50:14 +0900

    init

--- sample.rb
+++ sample.rb
@@ -1,14 +1,15 @@
 require "slack-notify"
 require 'clockwork'
 # ここに、slackにメッセージを送る処理を書き込む。
-sample = SlackNotify::Client.new(webhook_url: 'https://hooks.slack.com/services/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')
+sample = SlackNotify::Client.new(webhook_url: 'https://hooks.slack.com/services/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')
 
 sample.notify("Hello There!")
 
 require 'active_support/time'
+
 module Clockwork
 
-  sample = SlackNotify::Client.new(webhook_url: 'https://hooks.slack.com/services/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')
+  sample = SlackNotify::Client.new(webhook_url: 'https://hooks.slack.com/services/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')
 
   every(3.minutes, 'Hello There!') do
   sample.notify("Hello There!")
